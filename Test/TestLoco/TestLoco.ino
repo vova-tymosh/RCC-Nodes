5bba0919a08700a509d6117695ae12a49344f289
vova <volodymyr.tymoshchuk@gmail.com>
2024-11-23 10:14:02 -0800

    Fix code for loco testbed

--- Test/TestLoco/TestLoco.ino
+++ Test/TestLoco/TestLoco.ino
@@ -1,47 +1,34 @@
 /*
- * Railroad Tools/Libraries Test Script
+ * RCC Example Locomotive
  *
- * Test should output "Command X: Y", where X is command char and Y is float
- * value. It sends uptime (in ms), packet lost rate and a last command value
- * back to control station
  *
  */
-#define ARDUINO_MICRO 1
-#define ARDUINO_UNO 2
-#define ARDUINO_RFNANO 3
-#define ARDUINO_XIAO 4
-#define PLATFORM ARDUINO_RFNANO
-
 #include "Peripheral.h"
 #include "RCCLoco.h"
 #include "SpeedSensor.h"
 #include "Timer.h"
 
-
-// Change pins per the test board
-#if PLATFORM == ARDUINO_MICRO
-#define CE_PIN 19
-#define CSN_PIN 18
-#elif PLATFORM == ARDUINO_UNO
-#define CE_PIN 9
-#define CSN_PIN 10
-#elif PLATFORM == ARDUINO_RFNANO
-#define CE_PIN 10
-#define CSN_PIN 9
-#else
-#define CE_PIN 0
-#define CSN_PIN 0
+#if defined(ARDUINO_AVR_NANO)
+    #define CE_PIN 10
+    #define CSN_PIN 9
+#elif defined(ARDUINO_AVR_LEONARDO)
+    #define CE_PIN 19
+    #define CSN_PIN 18
+#elif defined(ARDUINO_ARCH_NRF52)
+    #define CE_PIN 0
+    #define CSN_PIN 0
 #endif
 
 const int NODE = 01;
 const char *NAME = "t001";
-Storage storage;
+// Storage storage;
 Wireless wireless(CE_PIN, CSN_PIN);
-const float inches_per_click = 1;
-SpeedSensor speed_sensor(7, inches_per_click);
-SpeedSensorBase *speed_sensor_ptr = &speed_sensor;
 Timer timer;
-Light led(2);
+Timer blinker(1000);
+
+Motor motor(7, 8);
+Light yellow(0);
+Light blue(9);
 
 class TestLoco : public RCCLoco
 {
@@ -50,56 +37,73 @@ public:
 
     void onFunction(char code, bool value)
     {
-        led.apply(state.lights);
-        Serial.println("Function: " + String((int)code) + "/" + String(value));
+        Serial.println("onFunction: " + String((int)code) + "/" + String(value));
+        if (code == 0)
+            yellow.apply(value);
+        if (code == 1)
+            blue.apply(value);
     }
 
     void onThrottle(uint8_t direction, uint8_t throttle)
     {
-        state.speed = throttle;
-        // state.distance += state.speed * 10;
+        // Serial.println("onThrottle: " + String(direction) + "/" + String(throttle));
+        motor.apply(direction, throttle);
     }
 };
-TestLoco loco(&wireless, NODE, NAME, &storage);
+TestLoco loco(&wireless, NODE, NAME, NULL);//&storage);
+
+
 
 void setupSerial()
 {
     Serial.begin(115200);
-    delay(50);
-    // for (int i = 0; i < 100 || !Serial; i++);
-    Serial.println(F("Started"));
+    delay(250);
+    Serial.println("Started");
 }
 
 void setup()
 {
     setupSerial();
-    storage.setup(VERSION);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+
+    // storage.setup(VERSION);
+    motor.setup();
+    yellow.setup();
+    blue.setup();
     loco.setup();
-    // loco.debug = true;
-    speed_sensor.setup();
-    led.setup();
-    led.apply(loco.state.lights);
     timer.start(100);
+    blinker.restart();
+
+    // loco.debug = true;
 }
 
 void loop()
 {
+    if (blinker.hasFired()) {
+        static bool flip = false;
+        flip = !flip;
+        if (flip) {
+            digitalWrite(LED_BUILTIN, HIGH);
+        } else {
+            digitalWrite(LED_BUILTIN, LOW);
+        }
+    }
+
+
     loco.loop();
-    speed_sensor.loop();
 
     if (timer.hasFired()) {
-        // loco.state.speed = speedSensor.getSpeed();
-        loco.state.distance = speed_sensor.getDistance();
+        loco.state.speed = 0;
+        loco.state.distance = 0;
         loco.state.battery = 71;
-        loco.state.temperature = 82;
-        loco.state.psi = 23;
+        loco.state.temperature = 83;
+        loco.state.psi = 24;
         if (loco.debug) {
             Serial.println("Loco:" + String(loco.state.throttle) + " " +
                         String(loco.state.direction) + " " +
                         String(loco.state.speed) + " " +
-                        String(loco.state.distance) + " " + String(loco.pid.p) +
-                        " " + String(loco.pid.i) + " " + String(loco.pid.d) +
-                        " " + String(loco.pid.upper));
+                        String(loco.state.distance));
         }
     }
 }
